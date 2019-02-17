467cfd624ff652f8ac55b907c6fea728f132ba06
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-02-17 16:41:38 +0100

    initial commit

---
+++ src/main/java/ups/dl/com/App.java
@@ -0,0 +1,13 @@
+package ups.dl.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
